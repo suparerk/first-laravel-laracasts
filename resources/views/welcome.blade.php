1bbdd2b5d2678ec84ff8bdc1a6e63ce3b28c9ef7
Suparerk <ch.suparerk@gmail.com>
2019-11-28 03:56:31 +0700

    Controllers 101

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('content')
     <h1>My {{ $foo }} website</h1>
-    @foreach ($tasks as $task)
+    {{-- @foreach ($tasks as $task)
         <li>{{ $task }}</li>
-    @endforeach
+    @endforeach --}}
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -10,24 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    $tasks = [
-        'Go to store',
-        'Go to the market',
-        'Go to work'
-    ];
-
-    return view('welcome', [
-        'tasks' => $tasks,
-        'foo' => 'foobar'
-    ]);
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'PagesController@contact');
